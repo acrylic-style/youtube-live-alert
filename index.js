90f30220439a40adfeaa4df6c265f7f490016b91
acrylic-style <me@acrylicstyle.xyz>
2020-02-27 01:39:26 +0900

    bug fix

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ app.all('/youtube_callback', (req, res) => {
   const { feed: { entry } } = xml2js(req.body, { compact: true })
   const video_id = entry['yt:videoId']
   const msg = new webhook.MessageBuilder()
-    .setName('yululiさんが動画/生放送をアップロードしました！')
-    .addField('URL', `https://youtu.be/${video_id}`)
-    .addField('チャンネルURL', entry.author.uri)
+    .setName('新しい動画/生放送がアップロードされました！')
+    .addField('URL', `https://youtu.be/${video_id._text}`)
+    .addField('チャンネルURL', entry.author.uri._text)
     .setTime()
   Hook.send(msg)
   res.status(200).send(JSON.stringify({status: 'ok'}))
