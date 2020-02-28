e52bb9acf84df6e95d0698d7d57a4db49337b4ce
acrylic-style <me@acrylicstyle.xyz>
2020-02-28 18:02:31 +0900

    add serverless (no need to open port) live alert

--- index.js
+++ index.js
@@ -1,24 +1,35 @@
 const webhook = require('webhook-discord')
-const env = require('dotenv-safe').config().parsed
-const Hook = new webhook.Webhook(env.WEBHOOK_URL)
-const express = require('express')
-const app = express()
 const { xml2js } = require('xml-js')
+const fetch = require('node-fetch')
+const mapping = require('./mapping')
+const start = Date.now()
 
-app.all('/', (req, res) => {
-  res.send('It (doens\'t) works!')
-})
+const cache = {}
 
-app.all('/youtube_callback', (req, res) => {
-  const { feed: { entry } } = xml2js(req.body, { compact: true })
+const send = (body, url) => {
+  let { feed: { entry } } = xml2js(body, { compact: true })
+  if (entry.constructor.name === 'Array') entry = entry[0]
   const video_id = entry['yt:videoId']
   const msg = new webhook.MessageBuilder()
-    .setName('新しい動画/生放送がアップロードされました！')
+    .setName('YouTube')
+    .setAvatar('https://img.acrylicstyle.xyz/upload/348431217/410151897/youtubelogo.png')
+    .setText('新しい動画/生放送が公開されました！')
     .addField('URL', `https://youtu.be/${video_id._text}`)
     .addField('チャンネルURL', entry.author.uri._text)
     .setTime()
+  const Hook = new webhook.Webhook(url)
   Hook.send(msg)
-  res.status(200).send(JSON.stringify({status: 'ok'}))
-})
+}
 
-app.listen(env.PORT)
+const check = async () => {
+  Object.keys(mapping).forEach(async k => {
+    const text = await fetch(`https://www.youtube.com/feeds/videos.xml?channel_id=${k}`).then(res => res.text())
+    if (cache[k] === text) return
+    cache[k] = text
+    if ((Date.now()-start) >= 1000*31) send(text, mapping[k])
+  })
+}
+
+setInterval(check, 1000*30) // check every 30 seconds
+
+check()
